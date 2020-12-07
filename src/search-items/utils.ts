f1c6d54c0efbba6b41c19dfd0c62a318da711a81
maksis <maksis@adrenaline-network.com>
2020-12-07 17:57:21 +0200

    Tabs -> spaces

--- src/search-items/utils.ts
+++ src/search-items/utils.ts
@@ -1,48 +1,48 @@
 
 const extrawords = [ 
-	'multisubs', 'multi', 'dvdrip', 'dvdr', 'real proper', 'proper', 'ultimate directors cut', 'directors cut', 'dircut', 'x264', 'x265', 'pal', 'complete', 'limited', 'ntsc', 'bd25',
-	'bd50', 'bdr', 'bd9', 'retail', 'bluray', 'nordic', '720p', '1080p', 'read nfo', 'dts', 'hdtv', 'pdtv', 'hddvd', 'repack', 'internal', 'custom', 'subbed', 'unrated', 'recut',
-	'extended', 'dts51', 'finsub', 'swesub', 'dksub', 'nosub', 'remastered', '2disc', 'rf', 'fi', 'swe', 'stv', 'r5', 'festival', 'anniversary edition', 'bdrip', 'ac3', 'xvid',
-	'ws', 'int' 
+  'multisubs', 'multi', 'dvdrip', 'dvdr', 'real proper', 'proper', 'ultimate directors cut', 'directors cut', 'dircut', 'x264', 'x265', 'pal', 'complete', 'limited', 'ntsc', 'bd25',
+  'bd50', 'bdr', 'bd9', 'retail', 'bluray', 'nordic', '720p', '1080p', 'read nfo', 'dts', 'hdtv', 'pdtv', 'hddvd', 'repack', 'internal', 'custom', 'subbed', 'unrated', 'recut',
+  'extended', 'dts51', 'finsub', 'swesub', 'dksub', 'nosub', 'remastered', '2disc', 'rf', 'fi', 'swe', 'stv', 'r5', 'festival', 'anniversary edition', 'bdrip', 'ac3', 'xvid',
+  'ws', 'int' 
 ];
 
 const yearReg = /((\[)?((19[0-9]{2})|(20[0-9]{2}))|(s[0-9]([0-9])?(e|d)[0-9]([0-9])?)|(Season(\.)[0-9]([0-9])?)).*/;
 
 export const cleanTitle = (searchTerm: string) => {
-	let ret = searchTerm.toLocaleLowerCase();
+  let ret = searchTerm.toLocaleLowerCase();
 
-	// Remove group name
-	{
-		const pos = ret.lastIndexOf('-');
-		if (pos !== -1) {
-			ret = ret.substr(0, pos);
-		}
-	}
+  // Remove group name
+  {
+    const pos = ret.lastIndexOf('-');
+    if (pos !== -1) {
+      ret = ret.substr(0, pos);
+    }
+  }
 
-	// Replace separator chars with spaces
-	{
-		ret = ret.replace(/\.|_/g, ' ');
-	}
+  // Replace separator chars with spaces
+  {
+    ret = ret.replace(/\.|_/g, ' ');
+  }
 
 
-	// Remove words after year/episode
-	{
-		const match = ret.search(yearReg);
-		if (match !== -1) {
-			ret = ret.substr(0, match);
-		}
-	}
+  // Remove words after year/episode
+  {
+    const match = ret.search(yearReg);
+    if (match !== -1) {
+      ret = ret.substr(0, match);
+    }
+  }
 
-	// Remove extra words
-	{
-		ret += ' ';
-		for (const extraWord of extrawords) {
-			ret = ret.replace(` ${extraWord} `, ' ');
-		}
-	}
+  // Remove extra words
+  {
+    ret += ' ';
+    for (const extraWord of extrawords) {
+      ret = ret.replace(` ${extraWord} `, ' ');
+    }
+  }
 
-	//trim spaces from the end
-	return ret.trim();
+  //trim spaces from the end
+  return ret.trim();
 }
 
 
@@ -72,30 +72,30 @@ const subDirReg = /^(((DVD|CD|DIS(K|C)).?([0-9](0-9)?))|Sample|Cover(s)?|.{0,5}S
 
 // Parse the last meaningful name from a directory path
 export const getDirectoryPathName = (directoryPath: string, separator: string) => {
-	if (directoryPath.length < 3) {
-		return directoryPath;
-	}
+  if (directoryPath.length < 3) {
+    return directoryPath;
+  }
 
-	// Get the directory to search for
-	let i = directoryPath.length - 2;
-	let j = 0;
-	for (;;) {
-		j = directoryPath.substr(0, i).lastIndexOf(separator);
-		if (j === -1) {
-			j = 0;
-			break;
-		}
+  // Get the directory to search for
+  let i = directoryPath.length - 2;
+  let j = 0;
+  for (;;) {
+    j = directoryPath.substr(0, i).lastIndexOf(separator);
+    if (j === -1) {
+      j = 0;
+      break;
+    }
 
-		const directory = directoryPath.substr(j + 1, i - j);
-		if (directory.search(subDirReg) === -1) {
-			j++;
-			break;
-		}
+    const directory = directoryPath.substr(j + 1, i - j);
+    if (directory.search(subDirReg) === -1) {
+      j++;
+      break;
+    }
 
-		i = j - 1;
-	}
+    i = j - 1;
+  }
 
-	return directoryPath.substr(j, i - j + 1);
+  return directoryPath.substr(j, i - j + 1);
 };
 
 export const ADC_PATH_SEPARATOR = '/';
--- src/tests/helpers.ts
+++ src/tests/helpers.ts
@@ -2,7 +2,7 @@ import { ItemInfoGetter, SearchItem } from 'src/types';
 
 
 import {
-	getMenuItems,
+  getMenuItems,
 } from 'src/search-items';
 
 import { getMockContext } from './mock-context';
@@ -29,28 +29,28 @@ export const MockSearchItems: SearchItem[] = [
 ];
 
 export const itemUrlParser = <IdT extends any, EntityIdT extends any>(
-	ids: IdT[],
-	entityId: EntityIdT
+  ids: IdT[],
+  entityId: EntityIdT
 ) => {
-	const parseUrls = (item: ContextMenuItem<IdT, EntityIdT>) => {
-		const { urls } = item;
-		if (Array.isArray(urls)) {
-			return urls;
-		}
+  const parseUrls = (item: ContextMenuItem<IdT, EntityIdT>) => {
+    const { urls } = item;
+    if (Array.isArray(urls)) {
+      return urls;
+    }
 
-		return urls!(ids, entityId, ["admin"], ["urls"]);
-	};
+    return urls!(ids, entityId, ["admin"], ["urls"]);
+  };
 
-	return parseUrls;
+  return parseUrls;
 };
 
 export const getUrls = async <IdT, EntityIdT>(
-	infoGetter: ItemInfoGetter<IdT, EntityIdT>,
+  infoGetter: ItemInfoGetter<IdT, EntityIdT>,
   itemParser: ReturnType<typeof itemUrlParser>
 ) => {
-	const context = getMockContext();
-	const items = getMenuItems(context, MockSearchItems, infoGetter);
+  const context = getMockContext();
+  const items = getMenuItems(context, MockSearchItems, infoGetter);
 
-	const urls = await Promise.all(items.map(itemParser));
-	return urls;
+  const urls = await Promise.all(items.map(itemParser));
+  return urls;
 };
